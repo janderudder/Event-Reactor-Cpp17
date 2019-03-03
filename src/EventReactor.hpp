f30d68ffaa3b6cc54101030b17aab6c940392441
janderudder <39810873+janderudder@users.noreply.github.com>
2019-03-03 01:39:23 +0100

    Removed base event class dependency by using const void* as callback argument.

--- src/EventReactor.hpp
+++ src/EventReactor.hpp
@@ -1,50 +1,19 @@
 #pragma once
 #include <map>
-#include <functional>
 #include <vector>
-#include <cstdint>
-#include <variant>
-#include <iterator>
-#include <type_traits>
-
-#include "Event.hpp"
+#include <functional>
 #include "getTypeId.hpp"
-/**
- * class EventReactor
- *
- * Register an arbitrary number of functions to be executed when a certain type
- * of event is received by the reactor.
- *
- * Two types of callbacks can be stored by this class:
- *
- *      - functions with no argument `void ()`
- *      - functions taking a pointer to Event as argument `void (const Event*)`
- *
- * A callback can be a free function (for example when passing a lambda),
- * or a member function, in which case it must be bound to an object instance:
- * the implicit `this` first argument of the method.
- *
- * This class maps event categories (id of type) to arrays of callbacks.
- * To enable the erasure of a callback from the vector, for example when a
- * corresponding object is destroyed, and therefore avoid dereferencing freed
- * memory, the `registerCallback` method will return an object containing the
- * information needed to locate the new entry at a later time.
- *
- * This object is a `std::pair<iterator, index>`, where `first` is an iterator
- * in the reactor's map (pointing to a vector), and `second` is the index of
- * the entry in the vector. An object can store and use this pair to delete its
- * related entry inside the EventReactor, via the `unsubscribe` method.
- *
- */
+
+
 class EventReactor
 {
-    using EventType_T       = TypeIdentifier;
-    using Callback_T        = std::function<void(const Event&)>;
-    using Map_T             = std::map<EventType_T, std::vector<Callback_T>>;
-    using EntryLocation_T   = std::pair<Map_T::iterator, std::size_t>;
+    using EventType_t       = TypeIdentifier;
+    using Callback_t        = std::function<void(const void*)>;
+    using Map_t             = std::map<EventType_t, std::vector<Callback_t>>;
+    using EntryLocation_t   = std::pair<Map_t::iterator, std::size_t>;
 
 
-    Map_T mCallbacks;
+    Map_t mCallbacks;
 
 
 public:
@@ -52,12 +21,12 @@ public:
      * Register free and static class functions.
      */
     template <class Event_T, class Func_T>
-    EntryLocation_T registerCallback(Func_T&& function)
+    EntryLocation_t registerCallback(Func_T&& function)
     {
         // Wrap the function in the storable function type if needed
         auto callback = ([&function] {
-            if constexpr (!std::is_convertible_v<Func_T, std::function<void(const Event&)>>) {
-                return [&function](const Event&){function();};
+            if constexpr (!std::is_convertible_v<Func_T, std::function<void(const void*)>>) {
+                return [&function](const void*){function();};
             }
             else return function;
         })();
@@ -65,30 +34,28 @@ public:
         return this->_insertCallback(getTypeId<Event_T>(), std::move(callback));
     }
 
-
     /**
      * Register class methods taking const Event& as argument.
      */
     template <class Event_T, class T>
-    EntryLocation_T registerCallback(void (T::*function)(const Event&), T& object)
+    EntryLocation_t registerCallback(void (T::*function)(const void*), T& object)
     {
         auto callback = std::bind(function, &object, std::placeholders::_1);
         return this->_insertCallback(getTypeId<Event_T>(), std::move(callback));
     }
 
-
     /**
      * Register class methods taking no argument.
      */
     template <class Event_T, class T>
-    EntryLocation_T registerCallback(void (T::*function)(), T& object)
+    EntryLocation_t registerCallback(void (T::*function)(), T& object)
     {
         auto callback = std::bind(function, &object);
         return this->_insertCallback(getTypeId<Event_T>(), std::move(callback));
     }
 
 
-    void eraseEntry(const EntryLocation_T& location)
+    void eraseEntry(const EntryLocation_t& location)
     {
         // Find the right vector.
         const auto [iterator, index] = location;
@@ -116,22 +83,22 @@ public:
 
         // Call all callbacks for this event type id.
         for (const auto& callback : std::as_const(it->second)) {
-            std::invoke(callback, event);
+            std::invoke(callback, static_cast<const void*>(&event));
         }
     }
 
 
 private:
     /**
-     * Used by registerCallback(...) overloads to effectively insert
+     * Used by `registerCallback` overloads to effectively insert
      * the callback into the instance's map and return the location
      * of the new entry.
      */
-    EntryLocation_T _insertCallback(EventType_T eventType, std::function<void(const Event&)>&& callback)
+    EntryLocation_t _insertCallback(EventType_t eventType, std::function<void(const void*)>&& callback)
     {
         // Get an iterator to the relevant array in the map.
         // It will be constructed in place now if it is not already there.
-        std::pair<Map_T::iterator, bool>
+        std::pair<Map_t::iterator, bool>
             insertionResult = mCallbacks.try_emplace(eventType);
 
         // The iterator gives us the array to insert to.
@@ -141,5 +108,6 @@ private:
         // Construct the returned object, to enable finding this entry later.
         return { insertionResult.first, vector.size() - 1 };
     }
+
 
 };
